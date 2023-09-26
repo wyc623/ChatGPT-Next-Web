51df45c09808c08b97787d0f23089dddd2d6823d
wyc623 <wyc.623@163.com>
2023-09-26 12:01:05 +0800

    Update route.ts

--- app/api/cors/[...path]/route.ts
+++ app/api/cors/[...path]/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export const runtime = 'edge';
+
 async function handle(
   req: NextRequest,
   { params }: { params: { path: string[] } },
